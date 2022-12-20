15d53de65e96b47455ff2bae2cce1c71bd4b0dfc
TobiPerel <pereltobias@gmail.com>
2022-12-19 23:02:48 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import * as IPFS from 'ipfs';
+import * as dataJson from "./data.json" assert { type: "json" } ; 
 
-async function main() {
-    const node = await IPFS.create()
+async function viewDatabase(path,node){
+    const stream = node.cat(path)
 
-    let data = 'Hello, Tobias Perel'
-
-    
-    const results = await node.add(data)
-    console.log(results.path)    
-
-    const stream = node.cat('QmTcGGKKH9rxo9LKK9w24DNQJkDDHNtfShcnqipoCgXvvs')
     const decoder = new TextDecoder()
-    data = ''
+    let data = ''
     for await (const chunk of stream) {
         // chunks of data are returned as a Uint8Array, convert it back to a string
         data += decoder.decode(chunk, { stream: true })
     }
     
     console.log(data)
+}
 
+async function main() {
+    console.log(dataJson);
+    const node = await IPFS.create()
+
+    const databaseBuffer = Buffer.from(JSON.stringify(dataJson))
+    const results = await node.add(databaseBuffer)
+
+    console.log(results.path) 
+    
+    viewDatabase(results.path, node)
+    
 }
 main()
